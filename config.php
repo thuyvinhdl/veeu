0edcbdc49378df0b2d020d3d926d4085d95b414f
Đức Thủy <itsme.nguyen@gmail.com>
2019-02-21 16:32:41 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$config = [
+	"30ba6ab7-2d3f-49c6-ba86-3ec0ae1aad8a",
+	"30ba6ab7-2d3f-49c6-ba86-3ec0ae1aad8a",
+	"30ba6ab7-2d3f-49c6-ba86-3ec0ae1aad8a",
+	"30ba6ab7-2d3f-49c6-ba86-3ec0ae1aad8a",
+
+
+
+];
